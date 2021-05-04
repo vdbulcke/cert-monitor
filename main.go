bbf1436a89b63358ff2d34148036e683dcaec7a7
vdbulcke <cedric.vandenbulcke.linux@gmail.com>
2021-05-04 18:13:44 +0200

    preparing release v0.2.0

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ import (
 var GitCommit string
 
 // Version
-var version = "0.1.0"
+var version = "0.2.0"
 
 // HumanVersion version with commit
 var HumanVersion = fmt.Sprintf("%s-(%s)", version, GitCommit)
