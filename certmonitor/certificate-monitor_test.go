5205c3276a4952cf294a14077318c28972ab1ecd
vdbulcke <cedric.vandenbulcke.linux@gmail.com>
2021-12-28 18:12:08 +0100

    fix: avoid import loop

--- certmonitor/certificate-monitor_test.go
+++ certmonitor/certificate-monitor_test.go
@@ -4,33 +4,7 @@ import (
 	"testing"
 
 	"github.com/hashicorp/go-hclog"
-	"github.com/vdbulcke/cert-monitor/ui"
 )
-
-func TestHttpClientGetCert(t *testing.T) {
-	var appLogger hclog.Logger
-
-	logLevel := hclog.LevelFromString("DEBUG")
-	appLogger = hclog.New(&hclog.LoggerOptions{
-		Name:       "cert-monitor",
-		Level:      logLevel,
-		JSONFormat: false,
-	})
-
-	emptyConfig := &Config{}
-	certMonitor := NewCertMonitor(appLogger, emptyConfig)
-
-	certs, err := certMonitor.getCertificateFromRemoteURL("certif.iamfas.belgium.be:443", "")
-	if err != nil {
-		t.Logf("Expected error wrong Format")
-		t.FailNow()
-	}
-
-	ui := ui.NewCertMonitorUI(appLogger, false, true)
-	ui.PrintX509CertList(certs, 90)
-
-	// t.FailNow()
-}
 
 func TestSAMLMetadata(t *testing.T) {
 
